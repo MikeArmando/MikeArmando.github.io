1995aa621cc6b77c525955d29946e2737497412f
Mike Armando <mike.armando.m@gmail.com>
2023-07-27 19:38:14 -0700

    Update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Gets the elements of the navbar
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementById('navbar-links')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 // Toggles on and off the navbar for smaller size screen
 toggleButton.addEventListener('click', () => {
+  console.log("hello")
   navbarLinks.classList.toggle('active')
 })
